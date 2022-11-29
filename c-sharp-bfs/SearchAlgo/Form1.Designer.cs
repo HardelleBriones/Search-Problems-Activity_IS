100c4a85069725f223b0f0d00ac9c8974a343d6c
Hardelle Briones <111712687+HardelleBriones@users.noreply.github.com>
2022-11-29 20:53:48 +0800

    Add files
    Initial commit

---
+++ c-sharp-bfs/SearchAlgo/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace SearchAlgo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblVisit = new System.Windows.Forms.Label();
+            this.lblBlockage = new System.Windows.Forms.Label();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.btnRandomizer = new System.Windows.Forms.Button();
+            this.numericUpDownBlockage = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBlockage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(79, 28);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            36,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(41, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(211, 28);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            36,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(41, 20);
+            this.numericUpDown2.TabIndex = 1;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Start:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Goal:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(111, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Find Path";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Click where to asign obstacle";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(191, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(61, 22);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Try Again";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblVisit
+            // 
+            this.lblVisit.AutoSize = true;
+            this.lblVisit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVisit.Location = new System.Drawing.Point(269, 28);
+            this.lblVisit.Name = "lblVisit";
+            this.lblVisit.Size = new System.Drawing.Size(172, 25);
+            this.lblVisit.TabIndex = 7;
+            this.lblVisit.Text = "Number of visits:";
+            this.lblVisit.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lblBlockage
+            // 
+            this.lblBlockage.AutoSize = true;
+            this.lblBlockage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBlockage.Location = new System.Drawing.Point(269, 72);
+            this.lblBlockage.Name = "lblBlockage";
+            this.lblBlockage.Size = new System.Drawing.Size(210, 25);
+            this.lblBlockage.TabIndex = 8;
+            this.lblBlockage.Text = "Number of blockage:";
+            // 
+            // lblPath
+            // 
+            this.lblPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPath.Location = new System.Drawing.Point(269, 109);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(199, 140);
+            this.lblPath.TabIndex = 9;
+            this.lblPath.Text = "Path:";
+            // 
+            // btnRandomizer
+            // 
+            this.btnRandomizer.Location = new System.Drawing.Point(274, 252);
+            this.btnRandomizer.Name = "btnRandomizer";
+            this.btnRandomizer.Size = new System.Drawing.Size(75, 23);
+            this.btnRandomizer.TabIndex = 10;
+            this.btnRandomizer.Text = "Randomizer";
+            this.btnRandomizer.UseVisualStyleBackColor = true;
+            this.btnRandomizer.Click += new System.EventHandler(this.btnRandomizer_Click);
+            // 
+            // numericUpDownBlockage
+            // 
+            this.numericUpDownBlockage.Location = new System.Drawing.Point(355, 255);
+            this.numericUpDownBlockage.Maximum = new decimal(new int[] {
+            36,
+            0,
+            0,
+            0});
+            this.numericUpDownBlockage.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownBlockage.Name = "numericUpDownBlockage";
+            this.numericUpDownBlockage.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDownBlockage.TabIndex = 11;
+            this.numericUpDownBlockage.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(509, 316);
+            this.Controls.Add(this.numericUpDownBlockage);
+            this.Controls.Add(this.btnRandomizer);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.lblBlockage);
+            this.Controls.Add(this.lblVisit);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBlockage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblVisit;
+        private System.Windows.Forms.Label lblBlockage;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Button btnRandomizer;
+        private System.Windows.Forms.NumericUpDown numericUpDownBlockage;
+    }
+}
+
